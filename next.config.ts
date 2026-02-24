2ff7fcecb864b5fe15266e779c15bf7e2ee8402c
= <=>
2026-02-24 15:17:23 +0600

    Improve Dashboard

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.pexels.com",
       },
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
     ],
   },
 };
